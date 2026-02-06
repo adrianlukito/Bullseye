5bb4e84b247ae8ab81fd99d9c2083b2ae2896a55
adrian.lukito <adrian.lukito@grabtaxi.com>
2026-02-06 21:25:53 +0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Adrian Lukito Lo on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Adrian Lukito Lo on 06/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
