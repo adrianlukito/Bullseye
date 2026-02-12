7edf13f009b5e55baf81d096ac21acaa4691114c
adrian.lukito <adrian.lukito@grabtaxi.com>
2026-02-12 10:48:57 +0700

    Bullseye Initial commit

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Adrian Lukito Lo on 08/02/26.
+//
+
+import Foundation
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,16 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Adrian Lukito Lo on 08/02/26.
+//
+
+import Testing
+
+struct BullseyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
