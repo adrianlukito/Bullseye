8a931ae9ac9cfafa1416acd3e807cbb4377fb09d
adrianlukito <lukito.adrian1@gmail.com>
2026-02-12 13:16:11 +0700

    Extract Views

---
+++ Bullseye/Views/TextViews.swift
@@ -0,0 +1,18 @@
+//
+//  TextViews.swift
+//  Bullseye
+//
+//  Created by Adrian Lukito Lo on 12/02/26.
+//
+
+import SwiftUI
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextViews()
+}
