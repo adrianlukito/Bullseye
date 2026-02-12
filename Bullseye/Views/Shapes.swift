4d41c144e3eae81640aa9b99bfc37fba65072130
adrianlukito <lukito.adrian1@gmail.com>
2026-02-12 13:31:24 +0700

    Fill and Stroke Shapes

--- Bullseye/Views/Shapes.swift
+++ Bullseye/Views/Shapes.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct Shapes: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Circle()
+                .strokeBorder(Color.blue, lineWidth: 20)
+                .frame(width: 200, height: 100)
+                .background(Color.green)
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width: 200, height: 100)
+            Capsule()
+                .fill(Color.blue)
+                .frame(width: 200, height: 100)
+            Ellipse()
+                .fill(Color.blue)
+                .frame(width: 200, height: 100)
+        }
     }
 }
 
