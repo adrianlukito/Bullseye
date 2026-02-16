cbf78493a0f9a295f19c1f98b83bb7a461b69696
adrianlukito <lukito.adrian1@gmail.com>
2026-02-16 10:36:08 +0700

    Display custom alert view

--- Bullseye/Views/PointsView.swift
+++ Bullseye/Views/PointsView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct PointsView: View {
+    @Binding var alertIsVisible: Bool
     @Binding var sliderValue: Double
     @Binding var game: Game
     
     var body: some View {
+        let roundedValue = Int(sliderValue.rounded())
+        let points = game.points(sliderValue: roundedValue)
         VStack(spacing: 10){
             InstructionText(text: "The slider's value is")
-            BigNumberText(text: String(Int(sliderValue)))
-            BodyText(text: "You scored \(game.points(sliderValue: Int(sliderValue))) Points\n 🎉🎉🎉")
+            BigNumberText(text: String(roundedValue))
+            BodyText(text: "You scored \(points) Points\n 🎉🎉🎉")
             Button(
                 action: {
-                    game.restart()
+                    alertIsVisible = false
+                    game.startNewRound(points: points)
                 },
                 label: {
                     ButtonText(text: "Start New Round")
@@ -36,10 +40,10 @@ struct PointsView: View {
 }
 
 #Preview {
-    PointsView(sliderValue: .constant(50.0), game: .constant(Game()))
+    PointsView(alertIsVisible: .constant(true), sliderValue: .constant(50.0), game: .constant(Game()))
 }
 
 #Preview(traits: .landscapeRight) {
-    PointsView(sliderValue: .constant(50.0), game: .constant(Game()))
+    PointsView(alertIsVisible: .constant(true), sliderValue: .constant(50.0), game: .constant(Game()))
         .preferredColorScheme(.dark)
 }
